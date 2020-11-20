3eddda22a204c9c5fcdfb3193990dde6a928880b
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-11-20 17:09:32 +0100

    Added tests for the join_base_url_and_query_string function.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -15,3 +15,37 @@ class TestPrepareTimestampQueryString:
         expected_query_string = "dateTime=2020-11-20T16:09:40Z"
         assert generated_query_string == expected_query_string
         # Cleanup - none
+
+
+class TestJoinBaseUrlAndQueryString:
+    def test_joining_of_combined_url_with_no_slash(self):
+        # Setup
+        base_url = (
+            "https://watchlistapi.icedatavault.icedataservices.com/v1/configurations/watchlists"
+        )
+        query_string = "dateTime=2020-11-20T16:09:40Z"
+        # Exercise
+        generated_url = helpers.join_base_url_and_query_string(base_url, query_string)
+        # Verify
+        expected_url = (
+            "https://watchlistapi.icedatavault.icedataservices.com/v1/configurations/watchlists"
+            "?dateTime=2020-11-20T16:09:40Z"
+        )
+        assert generated_url == expected_url
+        # Cleanup - none
+
+    def test_joining_of_combined_url_with_slash(self):
+        # Setup
+        base_url = (
+            "https://watchlistapi.icedatavault.icedataservices.com/v1/configurations/watchlists/"
+        )
+        query_string = "dateTime=2020-11-20T16:09:40Z"
+        # Exercise
+        generated_url = helpers.join_base_url_and_query_string(base_url, query_string)
+        # Verify
+        expected_url = (
+            "https://watchlistapi.icedatavault.icedataservices.com/v1/configurations/watchlists"
+            "?dateTime=2020-11-20T16:09:40Z"
+        )
+        assert generated_url == expected_url
+        # Cleanup - none
