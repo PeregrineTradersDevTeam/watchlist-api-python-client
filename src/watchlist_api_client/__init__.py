c7cc94220c0efd4ed69ca6ef12f2eb176d2adc65
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-11-20 19:00:40 +0100

    Added the config_retriever, data_structures and helpers modules to the __all__ variable.

--- src/watchlist_api_client/__init__.py
+++ src/watchlist_api_client/__init__.py
@@ -7,8 +7,7 @@ Watchlist API Client Library for Python
 """
 
 
-from watchlist_api_client import config_sender
-from watchlist_api_client import data_structures
+from watchlist_api_client import config_retriever, config_sender, data_structures, helpers
 
 
 __version__ = "0.1.0"
@@ -25,5 +24,7 @@ __coyright__ = "copyright (c) 2020 " + __author__
 
 __all__ = [
     "config_sender",
-    "data_structures"
+    "config_retriever",
+    "data_structures",
+    "helpers"
 ]
