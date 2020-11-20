9e3ad1fffc6b652c58377a501d8a37a433068bea
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-11-20 16:13:46 +0100

    Added test for the prepare_timestamp_query_string function.

--- tests/test_config_retriever.py
+++ tests/test_config_retriever.py
@@ -5,6 +5,20 @@ from watchlist_api_client import config_retriever
 from watchlist_api_client.data_structures import RetrievedConfig
 
 
+class TestPrepareTimestampQueryString:
+    def test_preparation_of_query_string(self):
+        # Setup
+        iso_formatted_timestamp = "2020-11-20T16:09:40Z"
+        # Exercise
+        generated_query_string = config_retriever.prepare_timestamp_query_string(
+            iso_formatted_timestamp
+        )
+        # Verify
+        expected_query_string = "dateTime=2020-11-20T16:09:40Z"
+        assert generated_query_string == expected_query_string
+        # Cleanup - none
+
+
 class TestInferTimestampFromRetrievedResponse:
     def test_inference_of_timestamp_from_url_without_query_string(
         self,
