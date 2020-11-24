389f3c51460fadb6223ce239adc4f52dd5bfff1d
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-11-24 16:31:40 +0100

    Added moduel level docstring.

--- src/watchlist_api_client/data_structures.py
+++ src/watchlist_api_client/data_structures.py
@@ -1,25 +1,17 @@
-"""A one line summary of the module or program, terminated by a period.
+"""Module containing user-defined data structures."""
 
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the module or program.  Optionally, it may also
-contain a brief description of exported classes and functions and/or usage
-examples.
-
-  Typical usage example:
-
-  foo = ClassFoo()
-  bar = foo.FunctionBar()
-"""
 from typing import Dict, List, NamedTuple, Union
 
 
 class RequestSummary(NamedTuple):
     """Stores the content of the request summary obtained after submitting a new configuration."""
+
     submission_time: str
     summary: Dict[str, Union[int, List[str]]]
 
 
 class RetrievedConfig(NamedTuple):
     """Stores the content of the configuration retrieved from the Watchlist API."""
+
     timestamp: str
     config_body: bytes
