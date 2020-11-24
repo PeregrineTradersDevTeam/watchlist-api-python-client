40e3d66dc4bb7909148627bb2846dcacc98d6839
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-11-24 16:21:09 +0100

    Added docstrings to the two named tuple classes.

--- src/watchlist_api_client/data_structures.py
+++ src/watchlist_api_client/data_structures.py
@@ -14,5 +14,12 @@ from typing import Dict, NamedTuple
 
 
 class RequestSummary(NamedTuple):
+    """Stores the content of the request summary obtained after submitting a new configuration."""
     submission_time: str
     summary: Dict
+
+
+class RetrievedConfig(NamedTuple):
+    """Stores the content of the configuration retrieved from the Watchlist API."""
+    timestamp: str
+    config_body: bytes
